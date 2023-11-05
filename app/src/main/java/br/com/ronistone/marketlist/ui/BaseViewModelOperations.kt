3a6987c4857df00bc5d5553e61aee6a8021091df
Ronistone Junior <ronistonejunior@gmail.com>
2023-11-05 17:42:28 -0300

    fixing delete items bug when select list and after delete items list must update

--- app/src/main/java/br/com/ronistone/marketlist/ui/BaseViewModelOperations.kt
+++ app/src/main/java/br/com/ronistone/marketlist/ui/BaseViewModelOperations.kt
@@ -8,15 +8,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 
 abstract class BaseViewModelOperations : ViewModel() {
 
+    private var joinJob: Job? = null
     var jobs: List<Job> = mutableListOf()
 
     fun processRequest(view: View, errorMessage: String, request: suspend () -> Unit) {
-        val job = CoroutineScope(Dispatchers.IO).launch {
+        val job =  CoroutineScope(Dispatchers.IO).launch {
             try {
-                request()
+                withTimeout(10_000) {
+                    request()
+                }
             } catch (e: Exception) {
                 if(this.isActive) {
                     onError(view, errorMessage, e)
@@ -36,10 +40,26 @@ abstract class BaseViewModelOperations : ViewModel() {
         Log.e("VIEW MODEL ERROR", "$message -- Exception handled: ${e.localizedMessage}", e)
     }
 
+    fun joinJobs(callback: () -> Unit){
+        if(joinJob?.isCompleted == false) {
+            joinJob!!.cancel()
+        }
+        joinJob = CoroutineScope(Dispatchers.IO).launch {
+            jobs.forEach {
+                it.join()
+            }
+            callback()
+        }
+    }
+
 
     override fun onCleared() {
         super.onCleared()
         jobs.filter { !it.isCompleted }
             .forEach { it.cancel() }
+
+        if(joinJob?.isCompleted == false) {
+            joinJob!!.cancel()
+        }
     }
 }
